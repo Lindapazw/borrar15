8cf41aa5bf9fdff3f20cdedfdb9aa90df105a8fc
Linda Paz <lindacsndelapaz@gmail.com>
2023-07-11 15:14:48 -0300

    add arrays

---
+++ assets/js/arreglos.js
@@ -0,0 +1,25 @@
+// arreglo con 10 elementos
+const arr = new Array(10);
+console.log(arr);
+
+// arreglo vacio
+const arr1 = [];
+console.log(arr1);
+
+// arreglo de videojuegos
+let videojuegos = ['Mario Bros', 'Sega', 'Nintendo Wii'];
+console.log({videojuegos});
+
+// traigo el primer valor
+console.log(videojuegos[0]);
+
+let arregloCosas = [
+    true,
+    123,
+    'Fernando',
+    ['hola','soy','linda']
+];
+
+// llamo al primer arreglo y al segundo
+console.log(arregloCosas[3][2]);
+
