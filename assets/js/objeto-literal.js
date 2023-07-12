f360726122336e598d9c52b6ece7996a660f53c0
Linda Paz <lindacsndelapaz@gmail.com>
2023-07-12 11:12:11 -0300

    add mas propiedades

--- assets/js/objeto-literal.js
+++ assets/js/objeto-literal.js
@@ -42,6 +42,13 @@ console.log('Ultimo traje: ', personaje.trajes[personaje.trajes.length -1]);
 delete personaje.edad;
 console.log(personaje);
 
+personaje.casado = true;
+
 // elementos pares
 const entriesPares = Object.entries(personaje);
 console.log(entriesPares);
+
+console.log(personaje);
+
+
+
