d1b56e382813f612ea14d77a02a2fb3b19ba4ae3
Linda Paz <lindacsndelapaz@gmail.com>
2023-07-12 11:48:26 -0300

    add function

---
+++ assets/js/funciones.js
@@ -0,0 +1,13 @@
+function saludar(){
+    // dentro de la funcion hay metodos
+    console.log("Hola Linda");
+}
+
+const saludar2 = function(){
+    console.log("Hola Linda 2");
+}
+
+
+
+saludar();
+saludar2();
