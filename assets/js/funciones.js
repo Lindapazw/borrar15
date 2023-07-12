981642386240c7947356b6cfe3358e9bf0da6220
Linda Paz <lindacsndelapaz@gmail.com>
2023-07-12 11:49:30 -0300

    add argumentos

--- assets/js/funciones.js
+++ assets/js/funciones.js
@@ -7,7 +7,12 @@ const saludar2 = function(){
     console.log("Hola Linda 2");
 }
 
+// argumentos
+function saludar3(nombre){
+    console.log('Hola ' + nombre);
+}
 
 
 saludar();
 saludar2();
+saludar3('Linda 3');
