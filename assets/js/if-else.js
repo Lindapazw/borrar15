ef8f471ffaf0e11301d1f441a920dd9c8d15330e
Linda Paz <lindacsndelapaz@gmail.com>
2023-07-12 15:53:18 -0300

    add tarea if-else

--- assets/js/if-else.js
+++ assets/js/if-else.js
@@ -22,3 +22,17 @@ if (dia === 3){
 }
 
 // tarea
+
+dia = 1;
+
+const diasObjeto = {
+    0: 'domingo',
+    1: 'lunes',
+    2: 'martes',
+    3: 'miercoles',
+    4: 'jueves',
+    5: 'viernes',
+    6: 'sabado',
+}
+
+console.log(diasObjeto[dia] || 'dia no definido');
