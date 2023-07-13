170011e3feb04f187870793689ad374ebc6cbfc9
Linda Paz <lindacsndelapaz@gmail.com>
2023-07-13 11:25:19 -0300

    add switch

---
+++ assets/js/switch.js
@@ -0,0 +1,15 @@
+const dias = 5;
+
+switch(dias){
+    case 0:
+        console.log('Domingo');
+        break;
+    case 1:
+        console.log('Lunes');
+        break;
+    case 2:
+        console.log('Martes');
+        break;
+    default:
+        console.log('No es Domingo, Lunes, ni Martes');
+}
