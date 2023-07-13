0c6cb4614fee0aa4671a1affa659b015c8cf8326
Linda Paz <lindacsndelapaz@gmail.com>
2023-07-13 12:19:51 -0300

    add Do while

--- assets/js/ciclos/while.js
+++ assets/js/ciclos/while.js
@@ -6,3 +6,13 @@ while (i < carros.length) {
     console.log( carros[i] );
     i++;
 }
+
+// DO WHILE
+// por lo menos ejecuta el bloque interno una vez
+
+j = 0;
+
+do {
+    console.log(carros[j]);
+    j++;
+} while(carros[j]);
