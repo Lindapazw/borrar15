5273e3944087a7085ef4c72135af762b74b6d6ea
Linda Paz <lindacsndelapaz@gmail.com>
2023-07-13 10:24:42 -0300

    add code to ternario

--- assets/js/ternario.js
+++ assets/js/ternario.js
@@ -1,9 +1,24 @@
 // los dias se semana abrimos a las 11
 // los fines de semana a las 9 
 
-const dia = 0;
-const horaActual = 10;
+const dia = 1;
+const horaActual = 11;
 
 let horaApertura;
 let mensaje;
 
+if( dia === 0 || dia === 6) {
+    console.log('Es fin de semana');
+    horaApertura = 9;
+} else {
+    console.log('es un dia de semana');
+    horaApertura = 11;
+}
+
+if(horaActual >= horaApertura) {
+    mensaje = 'Esta abierto';
+} else {
+    mensaje = `Está cerrado, hoy abrimos a las ${horaApertura}`;
+}
+
+console.log({horaApertura, mensaje});
