7815ae9552c9213911566b159f8845767090280a
Linda Paz <lindacsndelapaz@gmail.com>
2023-07-13 11:16:49 -0300

    add portip-ternario

---
+++ assets/js/protip-ternario.js
@@ -0,0 +1,7 @@
+const elMayor = (a,b) => (a > b) ? a : b;
+console.log( elMayor(10,15) );
+
+const tieneMenbresia = (miembro) => (miembro) ? '2 Dólares' : '10 Dólares';
+console.log( tieneMenbresia(false) );
+
+
