5e7b13ef8fbc7381f95d1678509e66b0da2694f3
Linda Paz <lindacsndelapaz@gmail.com>
2023-07-13 12:28:51 -0300

    add for tradicional

---
+++ assets/js/ciclos/for.js
@@ -0,0 +1,8 @@
+const heroes = ['batman', 'super man', 'aquaman'];
+
+console.warn('For tradicional');
+
+//  incializacion, condicion, incrementacion
+for( let i = 0;  i < heroes.length;  i++ ) {
+    console.log(heroes[i]);
+}
