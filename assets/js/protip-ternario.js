881b4d535523c962a8ade8fc6b8c30bb6ea1d320
Linda Paz <lindacsndelapaz@gmail.com>
2023-07-13 11:19:53 -0300

    add ternario anidado

--- assets/js/protip-ternario.js
+++ assets/js/protip-ternario.js
@@ -4,4 +4,10 @@ console.log( elMayor(10,15) );
 const tieneMenbresia = (miembro) => (miembro) ? '2 Dólares' : '10 Dólares';
 console.log( tieneMenbresia(false) );
 
+// ternario anidado
+
+const nota = 95;
+const grago =   nota >= 95 ? 'A+' : 
+                nota >= 85 ? 'B+' : 'F';
+console.log(nota,grago);
 
