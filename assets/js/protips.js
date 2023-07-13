4f074a7b802e80e784dc19df728cf3d06b86cb30
Linda Paz <lindacsndelapaz@gmail.com>
2023-07-13 10:14:12 -0300

    add finish protips

--- assets/js/protips.js
+++ assets/js/protips.js
@@ -11,9 +11,20 @@ console.log(persona);
 // cambiamos a funcion de flecha
 const crearPersona2 = (nombre, apellido) =>({nombre,apellido});
 
+
 // segunda funcion
 function imprimeArgumentos (){
     console.log(arguments);
 }
 
 imprimeArgumentos(10,true,false,'Hola');
+
+
+// tercera funcion
+// despues del spread no se puede poner nada
+const imprimeArgumentos2 = (edad, ...args) => {
+    console.log({edad, args});
+};
+
+imprimeArgumentos2(10,true,false,'Hola');
+
